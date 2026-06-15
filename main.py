4fbb622ada157573b4cdf489b37612f1eac335ee
songwu330 <5372677767ws@gmail.com>
2026-06-15 14:48:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hello")
+import yfinance as yf
+
+symbols = [
+    "513100.SS",
+    "159995.SZ",
+    "518880.SS"
+]
+
+for s in symbols:
+    try:
+        df = yf.download(s, period="1mo", auto_adjust=True, progress=False)
+        print(s, "rows =", len(df))
+    except Exception as e:
+        print(s, e)
